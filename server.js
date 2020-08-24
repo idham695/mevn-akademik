ba588b7f179138a7a1ef5c72fadcbece16160eee
idham kholid <idhamkholid965@gmail.com>
2020-08-24 14:20:01 +0700

    add router

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import database from "./app/config/db.js";
+import prodiRoutes from "./app/routes/ProdiRoutes";
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+const db = database.url;
+// Connect to Mongo
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }) // Adding new mongo url parser
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/prodi", prodiRoutes);
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Sistem akademik" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
